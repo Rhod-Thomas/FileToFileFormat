30fbcb54a044412c92317e10c97fd06646dbf028
Rhod-Thomas <40323386+Rhod-Thomas@users.noreply.github.com>
2018-07-02 20:34:27 +0100

    Initial commit
    Initial commit of the source and project files.

---
+++ fileToFileFormatting/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Data = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fileToWrite = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Format data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Data
+            // 
+            this.Data.Location = new System.Drawing.Point(216, 70);
+            this.Data.Name = "Data";
+            this.Data.Size = new System.Drawing.Size(357, 20);
+            this.Data.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "File to read";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "File to write";
+            // 
+            // fileToWrite
+            // 
+            this.fileToWrite.Location = new System.Drawing.Point(216, 126);
+            this.fileToWrite.Name = "fileToWrite";
+            this.fileToWrite.Size = new System.Drawing.Size(357, 20);
+            this.fileToWrite.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.fileToWrite);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Data);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Data;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox fileToWrite;
+    }
+}
+
---
+++ fileToFileFormatting/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+
+        private const int END_OF_FILE = -1, COMMA = 44, CARRIAGE_RETURN = 13, LINE_FEED = 10; 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string filename = Data.Text;
+            string filename2 = fileToWrite.Text; 
+
+            StringBuilder inData = new StringBuilder(); 
+            
+            if (filename != null)
+            {
+
+                FileStream F1 = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                FileStream F2 = new FileStream(filename2, FileMode.OpenOrCreate, FileAccess.Write);
+
+                try
+                {
+                    
+                    // F.write
+
+                    int intDataIn; 
+
+                    while ((intDataIn = F1.ReadByte()) != END_OF_FILE) //check for end of file
+                    {
+
+                        if (intDataIn == CARRIAGE_RETURN)
+                        {
+                            intDataIn = COMMA;
+                            F2.WriteByte((byte)intDataIn);
+                        }
+                        else
+                        {
+                            F2.WriteByte((byte)intDataIn);
+                        }
+
+                    }
+
+                    F1.Close();
+                    F2.Close();
+                }
+
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(ex.ToString());
+                    F1.Close();
+                    F2.Close();
+                }
+
+            }
+        }
+    }
+}
